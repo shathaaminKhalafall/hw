4d66c98e35134261a4271a6d2c71e440e13e6fe4
shathaaminKhalafall <72312523+shathaaminKhalafall@users.noreply.github.com>
2020-12-04 21:13:11 +0200

    Create hw2.js

---
+++ hw2.js
@@ -0,0 +1,70 @@
+var objectJ= require("./hw.json");
+ for (let i=0; i <objectJ.length; i++ ) {
+    for(let x=0; x<objectJ[i]. topping.length ; X++ ){
+       console.log(objectJ[i].topping[x]. type); }}
+
+console.log('--------------');
+
+for( let i=0 ; i <objectJ.length;i++) {
+   for(let x=0 ; x<objectJ[i].batters.batter.length; x++){
+      console.log(objectJ)[i].batters.batter[x].type);
+
+}} 
+console.log('--------------');
+
+var total = 0;
+
+for (var i=0;i <objectJ.length; i++) { 
+  total += objectJ[i].ppu;
+}
+
+var avg=total / objectJ.length;
+console.log("the ppu avg" + avg);
+console.log('--------------');
+var sum=e;
+
+for( let i=0 i<objectJ.length; i++){ 
+  console.log(object)[1].ppu); 
+  sum +=objectJ[i].ppu;
+}
+var totalsum = sum; 
+console.log( "the ppu sum *totalsum"+totalsum);
+
+console.log('--------------');
+
+function ObjectId(){
+
+console.log("all OBject id : ");
+
+for ( let i=0 ; i <objects.length; i++){
+
+console.log(objectJ)[i].id + " ");
+
+}}
+
+function batterId(){
+
+console.log("all batter id : ");
+
+for( let i=0; i <objects.length; i++){
+
+for(let x=0; x<object[i].batters.batter.length; X++){
+
+console.log(object][i].batters.batter[x].id + " ");
+}}
+}
+
+function toppingId(){
+
+console.log("all topping id : ");
+
+for( Let i=0 ;i objectJ.1ength; i++ ) {
+  for (let x=2 ; x<objectil topping.tength; x++){
+
+console.log(objectJ[i].toppingIx].id +" " );
+  }}}
+  ObjectId();
+  batterId();
+  topping();
+
+console.log('--------------');
